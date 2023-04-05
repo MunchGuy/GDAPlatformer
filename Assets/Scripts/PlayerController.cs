9659817f81a65a4721dd75f2ff9b897129b35d63
VAH110-EA-User <VAH110-EA-User@LibArts.ColoState.EDU>
2023-04-05 17:50:07 -0600

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -125,17 +125,6 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void FixedUpdate()
     {
